b1e005d02735e39864fb8c247d8dc71a66aad921
Lance Jenkin <lancejenkin@gmail.com>
2012-03-21 19:06:46 +0200

    Added support for magnetic links

--- js/add_torrent.js
+++ js/add_torrent.js
@@ -4,7 +4,7 @@
 
 function show_download_icon() {
   var links = [];
-
+  var magnet_links = [];
   // Find any anchor links that mention torrents.
   $('a[href*=torrent], a:contains(torrent)').each(function(i) {
     if(this === undefined) {
@@ -15,6 +15,15 @@ function show_download_icon() {
     if(this.href.search(/\/(download|get)\//) > 0 || this.href.search(/\.torrent$/) > 0) {
       links.push(this);
     }
+  });
+
+  // Find any anchor links that mention magnets.
+  $("a[href^=magnet]").each(function(i){
+    if(this === undefined){
+      return false;
+    }
+
+    magnet_links.push(this);
   });
 
   // For all the found links, add the little download icon.
@@ -29,25 +38,47 @@ function show_download_icon() {
     $(links[i]).after('<a class="deluge-icon" title="Download in Deluge!" href="' + links[i].href + '"><img src="' + icon + '" alt="Download in Deluge" style="border:0;" /></a>');
   }
 
+  // For all magnetic links, add the little magnet icon
+  var magnet_icon = chrome.extension.getURL('images/icons/magnet_16.png');
+  var magnet_icon_added = chrome.extension.getURL('images/icons/magnet_16_green.png');
+
+  for(var i = 0; i < magnet_links.length; i++){
+    // Check if the icon has already been added
+    if ($(magnet_links[i]).next(".deluge-magnet-icon").length > 0){
+      continue;
+    }
+
+    $(magnet_links[i]).after('<a class="deluge-magnet-icon" title="Download in Deluge!" href="' + magnet_links[i].href + '"><img src="' + magnet_icon + '" alt="Download in Deluge" style="border:0;" /></a>');
+
+  }
+
   // For all the new Deluge download links we need to send a message to the main
   // extension to perform the adding action (see background.html).
-  $('.deluge-icon').live('click', function() {
+  $('.deluge-icon, .deluge-magnet-icon').live('click', function() {
     var link = this;
-
     chrome.extension.sendRequest({ msg: 'add_torrent_from_url', url: this.href},
     function(response) {
       if(response.msg == 'success') {
-        $('img', link).attr('src', icon_added);
+        if(link.href.indexOf("magnet:") == 0){
+          $('img', link).attr('src', magnet_icon_added);
+        }else{
+          $('img', link).attr('src', icon_added);
+        }
       } else {
         console.log('deluge: failed to download torrent file.');
       }
     });
     return false;
   });
+
+  
 }
+
+
 
 chrome.extension.sendRequest({msg: 'enable_download_icon'}, function(response) {
   console.log(response);
+
   if(response == 'true') {
     show_download_icon();
   }
